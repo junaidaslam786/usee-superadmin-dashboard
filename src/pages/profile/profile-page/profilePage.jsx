3c79c793e604196284484517d831f5a12f551f29
Muhammad Junaid Aslam <15-10648@formanite.fccollege.edu.pk>
2023-08-22 14:36:33 +0500

    call to action

--- src/pages/profile/profile-page/profilePage.jsx
+++ src/pages/profile/profile-page/profilePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./profilePage.module.css";
 import AccountDetails from "../../../components/Profile/account-details/AccountDetails";
 import TimeZone from "../../../components/Profile/timezone/TimeZone";
@@ -8,28 +8,48 @@ import EmbeddedCode from "../../../components/Profile/embedded-code/EmbeddedCode
 import Layout from "../../../layouts/Layout";
 
 const ProfilePage = () => {
+  const refs = {
+    accountDetails: useRef(null),
+    timeZone: useRef(null),
+    changePassword: useRef(null),
+    embeddedCode: useRef(null),
+  };
+
+  const sections = [
+    {
+      id: "accountDetails",
+      label: "Account Details",
+      component: <AccountDetails />,
+    },
+    { id: "timeZone", label: "Timezone", component: <TimeZone /> },
+    {
+      id: "changePassword",
+      label: "Change Password",
+      component: <ChangePassword />,
+    },
+    { id: "embeddedCode", label: "Embedded code", component: <EmbeddedCode /> },
+  ];
+
+  const scrollToRef = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <Layout>
       <div className={styles.ProfileInfo}>
         <div className={styles.profileLinks}>
-          <div className={styles.AD}>
-            <p>Account Details</p>
-          </div>
-          <div>
-            <p>Timezone</p>
-          </div>
-          <div>
-            <p>Change Password</p>
-          </div>
-          <div className={styles.EC}>
-            <p>Embedded code</p>
-          </div>
+          {sections.map((section) => (
+            <div key={section.id} onClick={() => scrollToRef(refs[section.id])}>
+              <p>{section.label}</p>
+            </div>
+          ))}
         </div>
         <div className={styles.UserRegistration}>
-          <AccountDetails />
-          <TimeZone />
-          <ChangePassword />
-          <EmbeddedCode />
+          {sections.map((section) => (
+            <div key={section.id} ref={refs[section.id]}>
+              {section.component}
+            </div>
+          ))}
         </div>
       </div>
     </Layout>
